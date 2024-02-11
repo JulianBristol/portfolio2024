64da02770184b6354ae4dd224dea21ff9cdcd527
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2024-02-11 17:47:48 -0500

    Fix Profile image for small screens

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,5 +1,6 @@
+"use client"
 import Image from "next/image";
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import bokeh from "../../assets/BokehBGProfile.webp";
 import profileImg from "../../assets/JulianBristol.webp";
 import github from "../../assets/github.svg";
@@ -10,6 +11,27 @@ import linkedIn from "../../assets/linkedIn.svg";
 interface HeroProps {}
 
 const Hero: FC<HeroProps> = ({}) => {
+	const myTitles = ['DEVELOPER'/* , 'DESIGNER', 'SCIENTIST', 'STUDENT', 'TEACHER' */];
+	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*?"
+
+	useEffect(() => {
+		const title = document.getElementById("titles");
+
+	if (title){
+		title.onmouseover = (event: MouseEvent) => {
+			if (event.target){
+				(event.target as HTMLElement).innerText
+				.split("")
+				.map( letter => {
+					characters[Math.floor(Math.random() * 35)]
+				})
+				.join("");
+			}
+		}
+	}
+	},[])
+	
+
 	return (
 		<>
 			<div className="xs:hidden flex justify-center flex-col my-4">
@@ -24,19 +46,22 @@ const Hero: FC<HeroProps> = ({}) => {
 
 				<div className="flex flex-row mt-6 mx-2 mb-4 gap-4">
 					{/* image */}
-					<div className="rounded-[50px] w-[120%] relative overflow-hidden border-ghost border-[1.5px] shadow-md shadow-shadow-25">
+					<div className="rounded-[50px] w-[120%] h-[250px] max-h-[250px] my-auto relative overflow-hidden border-ghost border-[1.5px] shadow-md shadow-shadow-25">
 						<Image
 							src={bokeh}
 							alt="Personal Image background"
+							priority
 							fill
-							objectFit="cover"
+							sizes="(max-width: 320px) 45vw, 60vw"
+							className="object-cover"
 						/>
 						<Image
 							src={profileImg}
 							alt="Image of Julian Bristol"
+							priority
 							fill
-							objectFit="cover"
-							className="!left-2 !top-2 scale-110"
+							sizes="(max-width: 320px) 45vw, 60vw"
+							className="object-contain !top-5 scale-150"
 						/>
 					</div>
 
@@ -55,8 +80,8 @@ const Hero: FC<HeroProps> = ({}) => {
 						<section className="flex flex-col">
 							<div className="flex flex-row justify-around mb-[2px]">
 								<p>I am a... </p>
-								<span className="text-[16px] text-hope-500 leading-[18px] hover:cursor-pointer">
-									Developer |
+								<span id="titles" className="text-[12px] text-hope-500 leading-[18px] font-mono hover:cursor-pointer">
+									{myTitles}|
 								</span>
 							</div>
 							<a
@@ -91,7 +116,7 @@ const Hero: FC<HeroProps> = ({}) => {
 
 				{/* Social Links */}
 				<section className="mx-4 my-4">
-					<ol className="flex flex-row justify-between text-[12px] text-hope-500">
+					<ol className="flex flex-row justify-evenly text-[12px] text-hope-500">
 						<li>
 							<figure>
 							<a href="https://github.com/JulianBristol" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center">
