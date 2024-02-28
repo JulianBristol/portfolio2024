300a0e365c235ab9c0f42cb4876cbb296b0b629e
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2024-02-28 11:14:01 -0500

    Fix non-Scroll on Anchor tag while scrolling and cleaned up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,13 +17,9 @@ import Portfolio from "./components/Portfolio/Portfolio";
 
 export default function Home() {
 	/* TODO: 
-	* Change the cursor size based on screen size
-	
-	consider making another screen break for smaller than 320px for the hero section
+	* consider making another screen break for smaller than 320px for the hero section
 	consider adding a onClick for the technology badges
 	*/
-	const [isTouchDevice, setIsTouchDevice] = useState(false);
-	const [toggleCursor, setToggleCursor] = useState(false);
 	const [lastScroll, setLastScroll] = useState(0);
 	const [isLenisActive, setIsLenisActive] = useState(true);
 
@@ -51,333 +47,132 @@ export default function Home() {
 		});
 	}, []);
 
-	const changeLenis = (bool: boolean) => {
-		console.log("lenis was :", isLenisActive)
-		setIsLenisActive(bool);
-		console.log("toggling to :", isLenisActive)
-	  };
-
-	/* Setup Lenis - Smooth Scroll */
-	/* useEffect(() => {
-			const lenis = new Lenis();
-
-		lenis.on("scroll", ScrollTrigger.update);
-
-		gsap.ticker.add((time) => {
-			lenis.raf(time * 1000);
-		});
-
-		gsap.ticker.lagSmoothing(0);
-		if (!isLenisActive) {
-			lenis.stop();
-		} else {
-			lenis.start();
-		}
-	}, [isLenisActive]); */
 	useEffect(() => {
 		const lenis = new Lenis();
-	
+
 		const handleLenisScroll = (time: number) => {
-		  if (isLenisActive) {
-			lenis.raf(time * 1000);
-			ScrollTrigger.update();
-		  }
+			if (isLenisActive) {
+				lenis.raf(time * 1000);
+				ScrollTrigger.update();
+			}
 		};
-	
+
 		gsap.ticker.add(handleLenisScroll);
-	
+
 		gsap.ticker.lagSmoothing(0);
-	
+
 		return () => {
-		  gsap.ticker.remove(handleLenisScroll);
+			gsap.ticker.remove(handleLenisScroll);
 		};
-	  }, [isLenisActive]);
-	
-	  const toggleLenisScroll = (bool: boolean) => {
+	}, [isLenisActive]);
+
+	const toggleLenisScroll = (bool: boolean) => {
 		setIsLenisActive(bool);
-	  };
-	
+	};
 
-	/* Setup AnimeJS */
+	/* Toggle Lenis when User Clicks Links */
 	useEffect(() => {
-		const timeline = anime.timeline({
-			duration: 150, //in milliseconds
-			easing: "easeOutExpo",
-		});
-		timeline
-			.add({
-				/* Adds stroke-width change to the id outerCursorPath based on the toggleCursor useState */
-				targets: "#outerCursorPath",
-				strokeWidth: [{ value: `${toggleCursor ? "1" : "0.5"}` }],
-			})
-			.add({
-				/* Adds viewbox change to the id cursor-outer based on the toggleCursor useState */
-				targets: "#cursor-outer",
-				viewBox: [
-					{ value: `${toggleCursor ? "-0.5 -0.5 15 15" : "0 0 14 14"}` },
-				],
+		const links = document.querySelectorAll("a");
+
+		links.forEach((link) => {
+			link.addEventListener("click", (e) => {
+				toggleLenisScroll(false);
+				setTimeout(() => {
+					toggleLenisScroll(true);
+				});
 			});
-	}, [toggleCursor]);
-
-	/* Use event delegation so that each link and button has the effect */
-	const containerRef = useRef<HTMLDivElement>(null);
-
-	useEffect(() => {
-		const outer = document.getElementById("cursor-outer");
-
-		const handleHover = (event: MouseEvent) => {
-			/* Check if user is hovering over a button or link */
-			if (event.target) {
-				const hoverTarget = (event.target as HTMLElement).closest("button, a");
-
-				/* run function to change cursor if the hoverTarget is true */
-				if (hoverTarget) {
-					setToggleCursor((prevToggleCursor) => {
-						if (outer) {
-							outer.style.width = "32px";
-							outer.style.height = "32px";
-							outer.style.mixBlendMode = "darken";
-						}
-						return true;
-					});
-				} else {
-					if (toggleCursor) {
-						setToggleCursor((prevToggleCursor) => {
-							if (outer) {
-								outer.style.width = "12px";
-								outer.style.height = "12px";
-								outer.style.mixBlendMode = "normal";
-							}
-							return false;
-						});
-					}
-				}
-			}
-		};
-
-		// Attach the event listener to the container
-		const container = containerRef.current;
-		if (container) {
-			container.addEventListener("mouseover", handleHover);
-		}
-
-		// Cleanup: remove the event listener when the component is unmounted
-		return () => {
-			if (container) {
-				container.removeEventListener("mouseover", handleHover);
-			}
-		};
-	}, [toggleCursor]);
-
-	/* Initial check if user is using a touch device */
-	useEffect(() => {
-		const checkTouchDevice = () => {
-			setIsTouchDevice(
-				"ontouchstart" in window ||
-					navigator.maxTouchPoints > 0 ||
-					navigator.maxTouchPoints > 0
-			);
-		};
-
-		// Initial check when component mounts
-		checkTouchDevice();
-
-		// Check for touch device on resize
-		window.addEventListener("resize", checkTouchDevice);
-
-		// Unmount
-		return () => {
-			window.removeEventListener("resize", checkTouchDevice);
-		};
+		});
 	}, []);
 
-	/* mouse chaser */
+	/* Handle Header Display on Scroll Up */
 	useEffect(() => {
 		document.addEventListener("scroll", (e) => {
-		const scrollPos = window.scrollY;
-		setLastScroll((prev) => {
-			const header = document.getElementById("header")
-			
-			if (scrollPos <= 60){
-				header?.classList.remove("scroll-down")
-				header?.classList.add("scroll-up")
-			}
+			const scrollPos = window.scrollY;
+			setLastScroll((prev) => {
+				const header = document.getElementById("header");
 
-			if (scrollPos > prev && !header?.classList.contains("scroll-down") && scrollPos > 60){
-				header?.classList.remove("scroll-up")
-				header?.classList.add("scroll-down")
-			}
-
-			if (scrollPos < prev && header?.classList.contains("scroll-down")){
-				header?.classList.remove("scroll-down")
-				header?.classList.add("scroll-up")
-			}
-			return scrollPos;
-		  })
-		})
-
-		const inner = document.getElementById("cursor-inner");
-		const outer = document.getElementById("cursor-outer");
-
-		if (inner && outer && !isTouchDevice) {
-			const opacity = inner.style.opacity == "1" && outer.style.opacity == "1";
-
-			let innerX = 0,
-				innerY = 0,
-				outerX = 0,
-				outerY = 0,
-				offsetX = 0,
-				offsetY = 0;
-
-			document.addEventListener("scroll", (e) => {
-				offsetX = window.scrollX;
-				offsetY = window.scrollY;
-				setLastScroll((prev) => {
-					const header = document.getElementById("header")
-					if (offsetY <= 60){
-						header?.classList.remove("scroll-down")
-						header?.classList.add("scroll-up")
-					}
-
-					if (offsetY > prev && !header?.classList.contains("scroll-down") && offsetY > 60){
-						header?.classList.remove("scroll-up")
-						header?.classList.add("scroll-down")
-					}
-
-					if (offsetY < prev && header?.classList.contains("scroll-down")){
-						header?.classList.remove("scroll-down")
-						header?.classList.add("scroll-up")
-					}
-					return offsetY;
-				  })
-
-
-				inner.style.top = `${innerY + offsetY}px`;
-				inner.style.left = `${innerX + offsetX}px`;
-				if (!opacity) {
-					inner.style.opacity = outer.style.opacity = "1";
+				if (scrollPos <= 60) {
+					header?.classList.remove("scroll-down");
+					header?.classList.add("scroll-up");
 				}
-			});
 
-			document.addEventListener("mousemove", (e) => {
-				innerX = e.clientX;
-				innerY = e.clientY;
-				offsetX = window.scrollX;
-				offsetY = window.scrollY;
-				inner.style.top = `${innerY + offsetY}px`;
-				inner.style.left = `${innerX + offsetX}px`;
-				if (!opacity) {
-					inner.style.opacity = outer.style.opacity = "1";
+				if (
+					scrollPos > prev &&
+					!header?.classList.contains("scroll-down") &&
+					scrollPos > 60
+				) {
+					header?.classList.remove("scroll-up");
+					header?.classList.add("scroll-down");
 				}
-			});
 
-			document.body.addEventListener("mousedown", function () {
-				setToggleCursor((prevToggleCursor) => {
-					if (outer) {
-						outer.style.width = "32px";
-						outer.style.height = "32px";
-						outer.style.mixBlendMode = "darken";
-					}
-					return true;
-				});
+				if (scrollPos < prev && header?.classList.contains("scroll-down")) {
+					header?.classList.remove("scroll-down");
+					header?.classList.add("scroll-up");
+				}
+				return scrollPos;
 			});
-
-			document.body.addEventListener("mouseup", function () {
-				setToggleCursor((prevToggleCursor) => {
-					if (outer) {
-						outer.style.width = "12px";
-						outer.style.height = "12px";
-						outer.style.mixBlendMode = "normal";
-					}
-					return true;
-				});
-			});
-
-			const outerAnimation = () => {
-				const parts = 6;
-				outerX += (innerX - outerX + offsetX) / parts;
-				outerY += (innerY - outerY + offsetY) / parts;
-				outer.style.top = `${outerY}px`;
-				outer.style.left = `${outerX}px`;
-				window.requestAnimationFrame(outerAnimation);
-			};
-			window.requestAnimationFrame(outerAnimation);
-		}
-	}, [isTouchDevice]);
+		});
+	}, []);
 
 	return (
 		<div>
-		<div
-			className="bg-creme h-full min-h-[100vh] focus:ring-poppy text-ash font-medium"
-			ref={containerRef}
-		>
-			{/* On desktop device - mouse chaser */}
-			{/* {!isTouchDevice && (
-				<div>
-					<svg
-						id="cursor-outer"
-						width="14"
-						height="14"
-						viewBox="0 0 14 14"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							id="outerCursorPath"
-							d="M13.75 7C13.75 10.7279 10.7279 13.75 7 13.75C3.27208 13.75 0.25 10.7279 0.25 7C0.25 3.27208 3.27208 0.25 7 0.25C10.7279 0.25 13.75 3.27208 13.75 7Z"
-							fill="#D0D9C9"
-							fillOpacity="0.55"
-							stroke="#246036"
-							strokeWidth="0.5"
-						/>
-					</svg>
-					<Image id="cursor-inner" src={innerCircle} alt="" />
-				</div>
-			)} */}
+			<div className="bg-creme h-full min-h-[100vh] focus:ring-poppy text-ash font-medium">
+				<div className="bg-creme bgCubes h-full min-h-[100vh] px-2">
+					<div className="mx-2 sm:mx-auto py-2 border-x-poppy border-y-transparent h-full min-h-[100vh] border-[0.5px] xs:border-[2px] md:border-[3px] redBorderTransition max-w-[1500px]">
+						<header
+							id="header"
+							className="scroll-up sticky top-[0px] w-full z-50 transition-all"
+						>
+							<div className="w-full h-[15px] bg-creme"/>
+							<Header />
+						</header>
+						<main className="mt-[80px] xs:mt-[90px] md:mt-[110px]">
+							{/* hero section */}
+							<section className="mb-20">
+								<Hero />
+							</section>
 
-			<div className="bg-creme bgCubes h-full min-h-[100vh] px-2">
-				<div className="mx-2 sm:mx-auto py-2 border-x-poppy border-y-transparent h-full min-h-[100vh] border-[0.5px] xs:border-[2px] md:border-[3px] redBorderTransition max-w-[1500px]">
-					<header id="header" className="scroll-up fixed top-[10px] w-full max-w-[1500px] pr-[34px] xs:pr-[35px] sm:pr-[19px] md:pr-[21px] 2xl:pr-[4px] z-50 transition-all">
-						<Header />
-					</header>
-					<main className="mt-[80px] xs:mt-[90px] md:mt-[110px]">
-						{/* hero section */}
-						<section className="mb-20">
-							<Hero />
-						</section>
+							{/* Let's make something magical */}
+							<section className="mx-12">
+								<LetsMakeSomethingMagical />
+							</section>
 
-						{/* Let's make something magical */}
-						<section className="mx-12">
-							<LetsMakeSomethingMagical />
-						</section>
+							{/* about me section */}
+							<section id="about" className="mt-[16vh]">
+								<About toggleLenisScroll={toggleLenisScroll} />
+							</section>
 
-						{/* about me section */}
-						<section id="about" className="mt-[16vh]">
-							<About toggleLenisScroll={toggleLenisScroll} />
-						</section>
-
-						{/* history section */}
-						<section id="history" className="mt-[160px] md:mt-[180px] lg:mt-[220px]">
-							<History />
+							{/* history section */}
+							<section
+								id="history"
+								className="mt-[160px] md:mt-[180px] lg:mt-[220px]"
+							>
+								<History />
 							</section>
 
 							{/* Technologies section */}
-						<section id="tech"  className="mt-[40px] md:mt-[60px] lg:mt-[80px] mx-4 md:mx-6">
-							<Technologies />
-						</section>
+							<section
+								id="tech"
+								className="mt-[40px] md:mt-[60px] lg:mt-[80px] mx-4 md:mx-6"
+							>
+								<Technologies />
+							</section>
 
 							{/* Portfolio items  */}
-						<section id="portfolio" className="mt-[40px] md:mt-[60px] lg:mt-[80px] mx-2 md:mx-4 mb-20">
-							<Portfolio />
-						</section>
-					</main>
+							<section
+								id="portfolio"
+								className="mt-[40px] md:mt-[60px] lg:mt-[80px] mx-2 md:mx-4 mb-20"
+							>
+								<Portfolio />
+							</section>
+						</main>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div className="fadeB4Contact w-full h-[20px] absolute mt-[-20px]"/>
-		<section id="contact">
-			<Contact />
-						</section>
+			<div className="fadeB4Contact w-full h-[20px] absolute mt-[-20px]" />
+			<section id="contact">
+				<Contact />
+			</section>
 		</div>
 	);
 }
